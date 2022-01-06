0d8a3a3a6421f828e0f3ad95f9cc4cb172d70384
sumitnn <sumitnautiyal446@gmail.com>
2022-01-06 17:48:43 +0530

    change

--- project/settings.py
+++ project/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-77)36pl&3k6fk95jc2$#okb8!9*evcptjd9%wx#u*by_cv*tha'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'https://django-apii.herokuapp.com']
 CORS_ORIGIN_ALLOW_ALL = True
